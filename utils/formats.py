a5b1d081620cd7b393ac28d84495173694189f0d
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-08-25 11:47:08 -0600

    Adding more data format extensions

--- utils/formats.py
+++ utils/formats.py
@@ -1,5 +1,16 @@
 DATA_FORMATS = [
     '.csv',
     '.xlsx',
-    
+    '.xls',
+    '.npy',
+    '.json',
+    '.xml',
+    '.tsv',
+    '.ini',
+    '.proto',
+    '.db',
+    '.h5',
+    '.model',
+    '.pickle',
+    '.pkl'
 ]
